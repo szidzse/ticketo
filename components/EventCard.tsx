a6c1f3132812d8c2989537853961e1f08273f3c9
Sós Gellért <gellertsos@gmail.com>
2025-01-19 12:45:32 +0100

    implement getUserTicketForEvent query

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -10,6 +10,12 @@ const EventCard = ({ eventId }: { eventId: Id<"events"> }) => {
   const { user } = useUser();
   const router = useRouter();
   const event = useQuery(api.events.getById, { eventId });
+  const availability = useQuery(api.events.getEventAvailability, { eventId });
+
+  const userTicket = useQuery(api.tickets.getUserTicketForEvent, {
+    eventId,
+    userId: user?.id ?? "",
+  });
 
   return <div>event card</div>;
 };
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
 } from "convex/server";
 import type * as constants from "../constants.js";
 import type * as events from "../events.js";
+import type * as tickets from "../tickets.js";
 import type * as users from "../users.js";
 
 /**
@@ -28,6 +29,7 @@ import type * as users from "../users.js";
 declare const fullApi: ApiFromModules<{
   constants: typeof constants;
   events: typeof events;
+  tickets: typeof tickets;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
