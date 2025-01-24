4fc14e5697d89654d447521d271ce471bb8585fe
Sós Gellért <gellertsos@gmail.com>
2025-01-24 02:25:02 +0100

    implement form based on shadcn form

--- components/EventForm.tsx
+++ components/EventForm.tsx
@@ -11,6 +11,19 @@ import { useToast } from "@/hooks/use-toast";
 import { useStorageUrl } from "@/lib/utils";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().min(1, "Event name is required"),
@@ -105,7 +118,193 @@ const EventForm = ({ mode, initialData }: EventFormProps) => {
     }
   };
 
-  return <div>event form</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* Event Name Section */}
+        <div className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Event Description Section */}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Location Section */}
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Date Section */}
+          <FormField
+            control={form.control}
+            name="eventDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event Date</FormLabel>
+                <FormControl>
+                  <Input
+                    type="date"
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(
+                        e.target.value ? new Date(e.target.value) : null,
+                      );
+                    }}
+                    value={
+                      field.value
+                        ? new Date(field.value).toISOString().split("T")[0]
+                        : ""
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Price Section */}
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price per Ticket</FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <span className="absolute left-2 top-1/2 -translate-y-1/2">
+                      €
+                    </span>
+                    <Input
+                      type="number"
+                      {...field}
+                      onChange={(e) => field.onChange(Number(e.target.value))}
+                      className="pl-6"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Event Total Tickets Section */}
+          <FormField
+            control={form.control}
+            name="totalTickets"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Total Tickets Available</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    {...field}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Image Upload Section */}
+          <div className="space-y-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Event Image
+            </label>
+            <div className="mt-1 flex items-center gap-4">
+              {imagePreview || (!removedCurrentImage && currentImageUrl) ? (
+                <div className="relative w-32 aspect-square bg-gray-100 rounded-lg">
+                  <Image
+                    src={imagePreview || currentImageUrl!}
+                    alt="Preview"
+                    fill
+                    className="object-contain rounded-lg"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setSelectedImage(null);
+                      setImagePreview(null);
+                      setRemovedCurrentImage(true);
+                      if (imageInput.current) {
+                        imageInput.current.value = "";
+                      }
+                    }}
+                    className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600 transition-colors"
+                  >
+                    ×
+                  </button>
+                </div>
+              ) : (
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  ref={imageInput}
+                  className="block w-full text-sm text-gray-500
+                    file:mr-4 file:py-2 file:px-4
+                    file:rounded-full file:border-0
+                    file:text-sm file:font-semibold
+                    file:bg-blue-50 file:text-blue-700
+                    hover:file:bg-blue-100"
+                />
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Button
+          type="submit"
+          disabled={isPending}
+          className="w-full bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-900 text-white font-medium py-2 px-4 rounded-lg transition-all duration-200 flex items-center justify-center gap-2"
+        >
+          {isPending ? (
+            <>
+              <Loader2 className="w-4 h-4 animate-spin" />
+              {mode === "create" ? "Creating Event..." : "Updating Event..."}
+            </>
+          ) : mode === "create" ? (
+            "Create Event"
+          ) : (
+            "Update Event"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default EventForm;
