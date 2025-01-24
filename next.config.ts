98c7e9908f2a2b41e76542d92f2a9ef6bdb2b203
Sós Gellért <gellertsos@gmail.com>
2025-01-24 02:42:42 +0100

    change next.config to be able to work with images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "neat-vulture-496.convex.cloud",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
