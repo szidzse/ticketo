21ae5d1bb9c244ebcbeb193e7d695701f275d2c7
Sós Gellért <gellertsos@gmail.com>
2025-01-27 17:40:03 +0100

    implement getTicketWithDetails query

--- components/Ticket.tsx
+++ components/Ticket.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useStorageUrl } from "@/lib/utils";
 
 const Ticket = ({ ticketId }: { ticketId: Id<"tickets"> }) => {
+  const ticket = useQuery(api.tickets.getTicketWithDetails, { ticketId });
+  const user = useQuery(api.users.getUserById, {
+    userId: ticket?.userId ?? "",
+  });
+  const imageUrl = useStorageUrl(ticket?.event?.imageStorageId);
+
   return <div>Ticket</div>;
 };
 
--- convex/tickets.ts
+++ convex/tickets.ts
@@ -1,7 +1,7 @@
 import { query } from "@/convex/_generated/server";
 import { v } from "convex/values";
 
-// Finds a user's ticket to an event
+// Finds a user's ticket to an event.
 export const getUserTicketForEvent = query({
   args: { eventId: v.id("events"), userId: v.string() },
   handler: async (ctx, { eventId, userId }) => {
@@ -13,5 +13,24 @@ export const getUserTicketForEvent = query({
       .first();
 
     return ticket;
+  },
+});
+
+// This query retrieves the data of a ticket and the event data for that ticket.
+export const getTicketWithDetails = query({
+  args: { ticketId: v.id("tickets") },
+  handler: async (ctx, { ticketId }) => {
+    const ticket = await ctx.db.get(ticketId);
+
+    if (!ticket) {
+      return null;
+    }
+
+    const event = await ctx.db.get(ticket.eventId);
+
+    return {
+      ...ticket,
+      event,
+    };
   },
 });
