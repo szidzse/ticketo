4cefb8fd87f04891577ee69cc7fac097c3ebe90d
Sós Gellért <gellertsos@gmail.com>
2025-02-24 17:59:13 +0100

    implement RegisterForm

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -1,7 +1,135 @@
-import React from "react";
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { register } from "@/actions/auth/register";
+import CardWrapper from "@/components/auth/CardWrapper";
+import { FormSuccess } from "@/components/auth/FormSuccess";
+import { FormError } from "@/components/auth/FormError";
+import GoogleLogin from "@/components/auth/GoogleLogin";
 
 const RegisterForm = () => {
-  return <div>register form</div>;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof RegisterSchema>) => {
+    setLoading(true);
+    register(data).then((res) => {
+      if (res.error) {
+        setLoading(false);
+        setSuccess("");
+        setError(res.error);
+      }
+      if (res.success) {
+        setLoading(false);
+        setError("");
+        setSuccess(res.success);
+      }
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Create an account"
+      title="Register"
+      backButtonHref="/auth/login"
+      backButtonLabel="Already have an account"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="johndoe@email.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="John Doe" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="passwordConfirmation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormSuccess message={success} />
+          <FormError message={error} />
+          <Button type="submit" className="w-full" disabled={loading}>
+            {loading ? "Loading..." : "Register"}
+          </Button>
+        </form>
+      </Form>
+      <GoogleLogin />
+    </CardWrapper>
+  );
 };
 
 export default RegisterForm;
