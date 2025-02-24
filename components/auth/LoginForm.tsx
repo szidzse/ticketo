91ccce4516985a1e3ad4af2311941b05f3e8c4ad
Sós Gellért <gellertsos@gmail.com>
2025-02-24 18:00:06 +0100

    implement LoginForm

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,7 +1,103 @@
-import React from "react";
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { login } from "@/actions/auth/login";
+import CardWrapper from "@/components/auth/CardWrapper";
+import { FormError } from "@/components/auth/FormError";
+import GoogleLogin from "@/components/auth/GoogleLogin";
 
 const LoginForm = () => {
-  return <div>login form</div>;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof LoginSchema>) => {
+    setLoading(true);
+    login(data).then((res) => {
+      if (res?.error) {
+        setError(res?.error);
+        setLoading(false);
+      }
+      if (res.success) {
+        setError("");
+        setLoading(false);
+      }
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Log in to your account"
+      title="Login"
+      backButtonHref="/auth/register"
+      backButtonLabel="Create an account"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="johndoe@email.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <Button type="submit" className="w-full" disabled={loading}>
+            {loading ? "Loading..." : "Login"}
+          </Button>
+        </form>
+      </Form>
+      <GoogleLogin />
+    </CardWrapper>
+  );
 };
 
 export default LoginForm;
